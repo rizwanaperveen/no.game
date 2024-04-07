bb13e789dc5b71d52e22dceb534a87567d4f228d
rizwanaperveen <159348120+rizwanaperveen@users.noreply.github.com>
2024-04-07 11:31:52 +0500

    Add files via upload

---
+++ game.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const randomNumber = Math.floor(Math.random()*5);
+const answers = await inquirer.prompt([
+    {
+        name:"userGuessNmber",
+        type:"number",
+        message:"please guess a number between 1 to 5:",
+    }
+]);
+if (answers.userGuessNmber == randomNumber) {
+    console.log(chalk.green.italic("Congratulations!you guess right no."));
+}
+else{
+console.log(chalk.red.bold("You guessed wrong number,"))
+}
